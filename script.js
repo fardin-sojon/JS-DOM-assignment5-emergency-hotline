d3dbe6c729a8947a4506d9362589f9ca7dcf78fe
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-08-27 20:38:14 +0600

    Functionalities call & call history done

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function getElementsClass(id) {
 }
 // id Element function
 function getElementId(id){
-    const elements = document.getElementById(id);
-    return elements
+    const element = document.getElementById(id);
+    return element
 }
 
 // Heart Count
@@ -24,3 +24,49 @@ for(let hearticon of heartIcons){
     })
 }
 
+// Call 
+const callButton = document.getElementsByClassName("call-btn")
+
+for(let callbtn of callButton){
+    callbtn.addEventListener("click", function(){
+
+        const cardSubTitle = callbtn.parentNode.parentNode.children[1].children[1].innerText;
+
+        const cardHelplineNumber = callbtn.parentNode.parentNode.children[2].children[0].innerText
+        
+        if(coinBalance >= coinChange){
+            coinBalance = coinBalance - coinChange;
+            alert(`📞 Calling ${cardSubTitle} ${cardHelplineNumber},,,`);
+        }
+        else{
+            alert("❌ You don't have enough coins! You need at least 20 coins to make a call.")
+            return;
+        }
+        getElementId("coin-balance").innerText = coinBalance;
+        
+        const historyContainer = getElementId("card-hisroty-container");
+
+        const newHistoryCard = document.createElement("div")
+        newHistoryCard.innerHTML = `
+            <div class="flex flex-col md:flex-row justify-between items-center bg-[#fafafa] px-3 py-4 rounded-lg mt-4">
+              <div>
+                <h1 class="font-bold">${cardSubTitle}</h1>
+                <span class="text-[#5C5C5C]">${cardHelplineNumber}</span>
+              </div>
+              <div><span>11:36:58</span></div>
+            </div>
+        `
+        historyContainer.append(newHistoryCard)
+    })
+}
+// coins-charge
+let coinBalance = getElementId("coin-balance").innerText
+const coinChange = 20;
+
+
+
+getElementId("clear-btn")
+.addEventListener("click", function(){
+    const historyContainer = getElementId("card-hisroty-container")
+    historyContainer.innerHTML = "";
+})
