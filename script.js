78fc8ac898386a01c01a55504384c5c2fb177e3c
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-08-28 01:19:34 +0600

    Responsive Layout done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getElementsClass(id) {
     return element;
 }
 // id Element function
-function getElementId(id){
+function getElementId(id) {
     const element = document.getElementById(id);
     return element
 }
@@ -14,10 +14,10 @@ function getElementId(id){
 // Heart Count
 const heartIcons = getElementsClass("heart-icon")
 
-for(let hearticon of heartIcons){
-    hearticon.addEventListener("click", function(){
+for (let hearticon of heartIcons) {
+    hearticon.addEventListener("click", function () {
 
-        const heartCount =getElementId("heart-count").innerText
+        const heartCount = getElementId("heart-count").innerText
 
         const currentHeartLove = Number(heartCount) + 1;
         getElementId("heart-count").innerText = currentHeartLove;
@@ -32,33 +32,33 @@ const coinChange = 20;
 // Call 
 const callButton = document.getElementsByClassName("call-btn")
 
-for(let callbtn of callButton){
-    callbtn.addEventListener("click", function(){
+for (let callbtn of callButton) {
+    callbtn.addEventListener("click", function () {
 
         const cardSubTitle = callbtn.parentNode.parentNode.children[1].children[1].innerText;
 
         const cardHelplineNumber = callbtn.parentNode.parentNode.children[2].children[0].innerText
-        
-        if(coinBalance >= coinChange){
+
+        if (coinBalance >= coinChange) {
             coinBalance = coinBalance - coinChange;
             alert(`📞 Calling ${cardSubTitle} ${cardHelplineNumber},,,`);
         }
-        else{
+        else {
             alert("❌ You don't have enough coins! You need at least 20 coins to make a call.")
             return;
         }
         getElementId("coin-balance").innerText = coinBalance;
-        
+
         const historyContainer = getElementId("card-hisroty-container");
 
         const newHistoryCard = document.createElement("div")
         newHistoryCard.innerHTML = `
-            <div class="flex flex-col md:flex-row justify-between items-center bg-[#fafafa] px-3 py-4 rounded-lg mt-4">
-              <div>
-                <h1 class="font-bold">${cardSubTitle}</h1>
+            <div class="flex flex-col md:flex-col lg:flex-row justify-between items-center bg-[#fafafa] px-3 py-4 rounded-lg mt-4">
+              <div class="flex flex-col justify-center items-center">
+                <h1 class="font-bold text-[14px] md:text-[16px]">${cardSubTitle}</h1>
                 <span class="text-[#5C5C5C]">${cardHelplineNumber}</span>
               </div>
-              <div><span>${new Date().toLocaleTimeString()}</span></div>
+              <div><span class="text-[11px] md:text-sm">${new Date().toLocaleTimeString()}</span></div>
             </div>
         `
         historyContainer.append(newHistoryCard)
@@ -68,18 +68,18 @@ for(let callbtn of callButton){
 
 
 getElementId("clear-btn")
-.addEventListener("click", function(){
-    const historyContainer = getElementId("card-hisroty-container")
-    historyContainer.innerHTML = "";
-})
+    .addEventListener("click", function () {
+        const historyContainer = getElementId("card-hisroty-container")
+        historyContainer.innerHTML = "";
+    })
 
 
 
 // copy count
 const copyButton = getElementsClass("copy-btn")
 
-for(let copy of copyButton){
-    copy.addEventListener("click", function(){
+for (let copy of copyButton) {
+    copy.addEventListener("click", function () {
         const copyCount = getElementId("copy-count").innerText;
 
         const currentCopyNumber = Number(copyCount) + 1;
@@ -88,8 +88,11 @@ for(let copy of copyButton){
         const cardHelplineNumber = copy.parentNode.parentNode.children[2].children[0].innerText
 
         navigator.clipboard.writeText(cardHelplineNumber)
-        .then(() => {
-          alert("Number Copied: " + cardHelplineNumber);
-        })
+            .then(() => {
+                alert("Number Copied: " + cardHelplineNumber);
+            })
+            .catch(err => {
+                console.error("Failed to copy: ", err);
+            });
     })
 }
