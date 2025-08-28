c97dd1dd905ce33c450ce7c6b9ccb6c963eafff9
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-08-28 16:55:36 +0600

    google font added

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ for (let callbtn of callButton) {
         const newHistoryCard = document.createElement("div")
         newHistoryCard.innerHTML = `
             <div class="flex flex-col md:flex-col lg:flex-row justify-between items-center bg-[#fafafa] px-3 py-4 rounded-lg mt-4">
-              <div class="flex flex-col justify-center items-center">
+              <div class="flex flex-col items-center md:items-crnter lg:items-start">
                 <h1 class="font-bold text-[14px] md:text-[16px]">${cardSubTitle}</h1>
                 <span class="text-[#5C5C5C]">${cardHelplineNumber}</span>
               </div>
